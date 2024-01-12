138dce00b9f37f93b6245496c55cebfa4e66e5ac
Suraj Kumar <asurajkumar.88@gmail.com>
2024-01-12 23:26:34 +0530

    Modified source to test shellcheck

--- source.sh
+++ source.sh
@@ -2,12 +2,13 @@
 
 echo "This is a test script"
 test_var="oneliner"
+kimsim="kimil"
 
-if [[ test_var -eq "oneliner" ]]; then
+if [[ $test_var = "oneliner" ]]; then
     echo "This script will be used by BATS to test oneliner app functionality"
 fi
 note="This script is not to be ran separately. Only to be used by BATS"
-for words in ${note[@]}; do
+for words in ${note[*]}; do
     printf "%s   " "$words"
     sleep 0.2
 done
